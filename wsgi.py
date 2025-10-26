346e3735a079775fb132f6d4b06778498ebbc1f2
Simon <sfierbeck@web.de>
2025-10-26 22:09:57 +0100

    Clean up wsgi.py by removing unused imports and simplifying the application run configuration

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python3
-import sys
-import os
+from app import app
 
-# App-Verzeichnis zum Python-Pfad hinzufügen
-sys.path.insert(0, os.path.dirname(__file__))
-
-from app import app as application  # WSGI erwartet 'application'
+if __name__ == "__main__":
+    app.run()
