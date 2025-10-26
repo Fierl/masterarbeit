ae71805a975b8b1b9da0a9298d8be1dc0ea877c5
Simon <sfierbeck@web.de>
2025-10-26 18:02:02 +0100

    Refactor wsgi.py to set up WSGI application and adjust import paths

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from app import app
+#!/usr/bin/python3
+import sys
+import os
 
-if __name__ == "__main__":
-    app.run()
+# App-Verzeichnis zum Python-Pfad hinzufügen
+sys.path.insert(0, os.path.dirname(__file__))
+
+from app import app as application  # WSGI erwartet 'application'
