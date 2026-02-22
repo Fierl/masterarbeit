c8d1c1dc8b91cbb902f40236ef09bead4706e954
Simon <sfierbeck@web.de>
2026-02-22 16:55:27 +0100

    fixed creation script

--- helper_scripts/create_user.py
+++ helper_scripts/create_user.py
@@ -5,18 +5,18 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1]))
 
 from app import app
 from src.database import db
-import src.models.User as _u
+from src.models import User
 
 def create_user():
     with app.app_context():
-        username = "ober"
-        raw_password = "sicheres_passwort_2025"
+        username = ""
+        raw_password = ""
 
-        if _u.User.query.filter_by(username=username).first():
+        if User.query.filter_by(username=username).first():
             print("User already exists:", username)
             return
 
-        user = _u.User(username=username) # type: ignore
+        user = User(username=username) # type: ignore
         user.set_password(raw_password)
         
         db.session.add(user)
